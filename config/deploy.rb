7e77d1654c79af51e6aef611bc4abd07f71c997a
a-know <a.know.3373@gmail.com>
2015-12-28 13:51:41 +0900

    Set application name and repos

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'a-know-home'
+set :repo_url, 'git@github.com:a-know/a-know-home-rails.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
