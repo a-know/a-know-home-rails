c0117b6105fe43f49d2f2d00d517d0574b7ed1d4
a-know <a.know.3373@gmail.com>
2016-01-03 14:52:21 +0900

    Implement api

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,28 @@
+require 'rss'
+
 class BookmarksController < ActionController::API
   def index
-    render json: { entries: nil }
+    feed_url = URI.parse('http://b.hatena.ne.jp/a-know/rss')
+    feed_url.query = "time=#{Time.now.to_i.to_s}" # against cache
+
+    feed = ''
+    Net::HTTP.start(feed_url.host, feed_url.port) do |http|
+      feed = http.get(feed_url.path, 'User-Agent' => 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01' ).body
+    end
+
+    rss = RSS::Parser.parse(feed)
+
+    render json: { entries: [].tap do |entries|
+      rss.items.each do |item|
+        entries << {}.tap do |entry|
+          entry[:comment] = item.description.gsub(/[\r\n]/,"")
+          entry[:date] = item.date.strftime('%Y-%m-%d-%H').gsub(/[\r\n]/,"")
+          entry[:hatebu_url] = "http://b.hatena.ne.jp/entry/#{item.link}".gsub(/[\r\n]/,"")
+          entry[:target_title] = item.title.gsub(/[\r\n]/,"")
+          entry[:target_url] = item.link.gsub(/[\r\n]/,"")
+        end
+      end
+    end }
+
   end
 end
