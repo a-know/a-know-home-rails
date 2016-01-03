dd558d90408cbc9097cc91aec5c166e7ddd5cc02
a-know <a.know.3373@gmail.com>
2016-01-03 13:32:39 +0900

    Fix routing spec

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'routing to bookmarks', type: :routing do
   it 'get /bookmarks to bookmarks#index' do
     expect(get: '/bookmarks').
-      to route_to(controller: 'bookmarks', action: 'index', format: 'json')
+      to route_to(controller: 'bookmarks', action: 'index')
   end
 end
