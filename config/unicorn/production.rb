eca42d0f119645362a96d922d2bf7d318ed3f2bc
a-know <a.know.3373@gmail.com>
2016-01-04 16:50:30 +0900

    Add configuration for production

---
+++ config/unicorn/production.rb
@@ -0,0 +1,35 @@
+# paths
+app_path = "/var/www/a-know-home"
+working_directory "#{app_path}/current"
+pid               "#{app_path}/current/tmp/pids/unicorn.pid"
+
+# listen
+listen "#{app_path}/shared/tmp/sockets/unicorn.socket", backlog: 64
+
+# logging
+stderr_path "log/unicorn.stderr.log"
+stdout_path "log/unicorn.stdout.log"
+
+# workers
+worker_processes 3
+
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
+end
+
+# preload
+preload_app true
+
+before_fork do |server, worker|
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
