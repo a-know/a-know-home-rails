2bcd13b5c908de871ebc2543508290605345adea
a-know <a.know.3373@gmail.com>
2016-02-05 20:33:52 +0900

    Refactor a little

--- app/controllers/blog_metricks_controller.rb
+++ app/controllers/blog_metricks_controller.rb
@@ -1,6 +1,14 @@
 class BlogMetricksController < ActionController::API
+  HATEDA_RSS = 'http://d.hatena.ne.jp/a-know/rss'.freeze
+  HATEBLO_FEED = 'http://blog.a-know.me/feed'.freeze
+  HATEBLO_RSS = 'http://blog.a-know.me/rss'.freeze
+
+  LDR_ENDPOINT = 'http://rpc.reader.livedoor.com/count?feedlink='.freeze
+
+  DUMMY_UA = 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01'
+
   def count_bookmarks
-    response_header = `curl -i -A 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01' http://b.hatena.ne.jp/bc/gr/http://blog.a-know.me/`
+    response_header = `curl -i -A '#{DUMMY_UA}' http://b.hatena.ne.jp/bc/gr/http://blog.a-know.me/`
     response_header =~ /Location: (.+)\r\n/
     image_url = $1
 
@@ -19,20 +27,26 @@ class BlogMetricksController < ActionController::API
   end
 
   def count_subscribers
-    ldr_hateda = ldr_check(Net::HTTP.get(URI.parse('http://rpc.reader.livedoor.com/count?feedlink=' + 'http://d.hatena.ne.jp/a-know/rss')).to_i)
-    ldr_hateblo_feed = ldr_check(Net::HTTP.get(URI.parse('http://rpc.reader.livedoor.com/count?feedlink=' + 'http://blog.a-know.me/feed')).to_i)
-    ldr_hateblo_rss  = ldr_check(Net::HTTP.get(URI.parse('http://rpc.reader.livedoor.com/count?feedlink=' + 'http://blog.a-know.me/rss')).to_i)
+    ldr_hateda = ldr_check(Net::HTTP.get(URI.parse(LDR_ENDPOINT + HATEDA_RSS)).to_i)
+    ldr_hateblo_feed = ldr_check(Net::HTTP.get(URI.parse(LDR_ENDPOINT + HATEBLO_FEED)).to_i)
+    ldr_hateblo_rss  = ldr_check(Net::HTTP.get(URI.parse(LDR_ENDPOINT + HATEBLO_RSS)).to_i)
 
     # http://cloud.feedly.com/v3/feeds/feed%2Fhttp%3A%2F%2Fd.hatena.ne.jp%2Fa-know%2Frss
-    feedly_hateda = JSON.parse(Net::HTTP.get(URI.parse(feedly_target('http://d.hatena.ne.jp/a-know/rss'))))['subscribers']
-    feedly_hateblo_feed = JSON.parse(Net::HTTP.get(URI.parse(feedly_target('http://blog.a-know.me/feed'))))['subscribers']
-    feedly_hateblo_rss  = JSON.parse(Net::HTTP.get(URI.parse(feedly_target('http://blog.a-know.me/rss'))))['subscribers']
+    feedly_hateda = JSON.parse(Net::HTTP.get(URI.parse(feedly_target(HATEDA_RSS))))['subscribers']
+    feedly_hateblo_feed = JSON.parse(Net::HTTP.get(URI.parse(feedly_target(HATEBLO_FEED))))['subscribers']
+    feedly_hateblo_rss  = JSON.parse(Net::HTTP.get(URI.parse(feedly_target(HATEBLO_RSS))))['subscribers']
 
-    hateblo_subscribers_response = `curl -A 'Opera/9.80 (Windows NT 5.1; U; ja) Presto/2.7.62 Version/11.01' http://blog.hatena.ne.jp/a-know/a-know.hateblo.jp/subscribe/iframe`
+    hateblo_subscribers_response = `curl -A '#{DUMMY_UA}' http://blog.hatena.ne.jp/a-know/a-know.hateblo.jp/subscribe/iframe`
     hateblo_subscribers_response =~ /data-subscribers-count="(\d+)"/
     hateblo_subscribers = $1.to_i
 
-    total_subscribers = ldr_hateda + ldr_hateblo_feed + ldr_hateblo_rss + feedly_hateda + feedly_hateblo_feed + feedly_hateblo_rss + hateblo_subscribers
+    total_subscribers = ldr_hateda +
+                        ldr_hateblo_feed +
+                        ldr_hateblo_rss +
+                        feedly_hateda +
+                        feedly_hateblo_feed +
+                        feedly_hateblo_rss +
+                        hateblo_subscribers
 
     logger = if Rails.env == 'test'
                Fluent::Logger::TestLogger.new('blog-metricks')
