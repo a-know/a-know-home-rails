7b34d120d388728b22a8ed9e47482afa036d5432
a-know <a.know.3373@gmail.com>
2016-02-07 07:01:25 +0900

    Refactor routing spec

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -1,44 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe 'routing to bookmarks', type: :routing do
-  it 'get /bookmarks to bookmarks#index' do
-    expect(get: '/bookmarks').
-      to route_to(controller: 'bookmarks', action: 'index')
+RSpec.describe 'Routes', type: :routing do
+  describe 'for a-know-home routes' do
+    it 'get /bookmarks to bookmarks#index' do
+      expect(get: '/bookmarks').
+        to route_to(controller: 'bookmarks', action: 'index')
+    end
+
+    it 'post /knock to knock#notify' do
+      expect(post: '/knock').
+        to route_to(controller: 'knock', action: 'notify')
+    end
+  end
+
+  describe 'for blog-metricks' do
+    it 'get /blog_metricks/bookmarks to blog_metricks#count_bookmarks' do
+      expect(get: '/blog_metricks/bookmarks').
+        to route_to(controller: 'blog_metricks', action: 'count_bookmarks')
+    end
+
+    it 'get /blog_metricks/subscribers to blog_metricks#count_subscribers' do
+      expect(get: '/blog_metricks/subscribers').
+        to route_to(controller: 'blog_metricks', action: 'count_subscribers')
+    end
+
+    it 'get /blog_metricks/hatena_stars to blog_metricks#count_hatena_stars' do
+      expect(get: '/blog_metricks/hatena_stars').
+        to route_to(controller: 'blog_metricks', action: 'count_hatena_stars')
+    end
+
+    it 'get /blog_metricks/active_visitors to blog_metricks#count_active_visitors' do
+      expect(get: '/blog_metricks/active_visitors').
+        to route_to(controller: 'blog_metricks', action: 'count_active_visitors')
+    end
   end
 end
-
-RSpec.describe 'routing to knock', type: :routing do
-  it 'post /knock to knock#notify' do
-    expect(post: '/knock').
-      to route_to(controller: 'knock', action: 'notify')
-  end
-end
-
-RSpec.describe 'routing to count bookmarks', type: :routing do
-  it 'get /blog_metricks/bookmarks to blog_metricks#count_bookmarks' do
-    expect(get: '/blog_metricks/bookmarks').
-      to route_to(controller: 'blog_metricks', action: 'count_bookmarks')
-  end
-end
-
-RSpec.describe 'routing to count subscribers', type: :routing do
-  it 'get /blog_metricks/subscribers to blog_metricks#count_subscribers' do
-    expect(get: '/blog_metricks/subscribers').
-      to route_to(controller: 'blog_metricks', action: 'count_subscribers')
-  end
-end
-
-RSpec.describe 'routing to count hatena-star', type: :routing do
-  it 'get /blog_metricks/hatena_stars to blog_metricks#count_hatena_stars' do
-    expect(get: '/blog_metricks/hatena_stars').
-      to route_to(controller: 'blog_metricks', action: 'count_hatena_stars')
-  end
-end
-
-RSpec.describe 'routing to count active visitors', type: :routing do
-  it 'get /blog_metricks/active_visitors to blog_metricks#count_active_visitors' do
-    expect(get: '/blog_metricks/active_visitors').
-      to route_to(controller: 'blog_metricks', action: 'count_active_visitors')
-  end
-end
-
