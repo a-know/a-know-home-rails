68c3efe453c6d74230889808ee4702d2ee7052c1
a-know <a.know.3373@gmail.com>
2016-02-07 07:41:43 +0900

    Send fitbit steps to fluentd

--- app/controllers/activity_metricks_controller.rb
+++ app/controllers/activity_metricks_controller.rb
@@ -1,4 +1,14 @@
 class ActivityMetricksController < SendToFluentController
   def collect_steps
+    return if send_fitbit_timing?
+
+    fitbit = Fitbit.new unit_system: 'METRIC'
+    return if fitbit.errors?
+
+    fluent_logger('a-know-metricks').post('activity',
+      {
+        todays_steps: fitbit.todays_steps,
+      }
+    )
   end
 end
--- app/controllers/send_to_fluent_controller.rb
+++ app/controllers/send_to_fluent_controller.rb
@@ -1,5 +1,6 @@
 class SendToFluentController < ActionController::API
   SEND_FREQUENCY_MIN = 15
+  SEND_FREQUENCY_FITBIT = 5
 
   def fluent_logger(primary_tag)
     @logger ||= if Rails.env == 'test'
@@ -14,5 +15,10 @@ class SendToFluentController < ActionController::API
   def every_15min?
     min = DateTime.now.min
     min % SEND_FREQUENCY_MIN == 0
+  end
+
+  def send_fitbit_timing?
+    min = DateTime.now.min
+    min % SEND_FREQUENCY_FITBIT == 0
   end
 end
