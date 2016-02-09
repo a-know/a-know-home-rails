850e0f04d55d228413e407374faa758ecdf17bef
a-know <a.know.3373@gmail.com>
2016-02-09 09:40:32 +0900

    Transplant grass-graph

---
+++ spec/request/grass_graph_spec.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require 'rails_helper'
+
+RSpec.describe '/grass_graph', type: :request do
+  describe 'GET /grass_graph' do
+    pending 'GitHub public contributions の画像を返す'
+  end
+end
