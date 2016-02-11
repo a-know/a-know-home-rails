f0f686c1d293759ab224d8e7ef9d43ad952284e2
a-know <a.know.3373@gmail.com>
2016-02-11 17:04:59 +0900

    Refactor

---
+++ spec/request/grass_graph_service.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require 'rails_helper'
+
+RSpec.describe '/images', type: :request do
+  describe 'GET /images' do
+    pending '指定された GitHub ユーザーの GitHub public contributions の画像を返す'
+  end
+end
