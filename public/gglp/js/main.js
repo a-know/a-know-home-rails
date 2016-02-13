31505b1d0aa9351983d9ab1520548e8c8a4dcb69
a-know <a.know.3373@gmail.com>
2016-02-13 10:19:55 +0900

    Generate personal img tag by input

--- public/gglp/js/main.js
+++ public/gglp/js/main.js
@@ -1,64 +1,64 @@
-$(document).ready(function() { 
+$(document).ready(function() {
 
 	"use strict";
-	
-	
+
+
 	 //PRELOADER
-     $(window).load(function() {    
+     $(window).load(function() {
         $("#status").fadeOut();
         $("#preloader").delay(1000).fadeOut("slow");
      });
-	
-	 //COUNTDOWN  
+
+	 //COUNTDOWN
 	 $('#clock').countdown('2016/10/10', function(event) {
 	   $(this).html(event.strftime('%D days'));
 	 });
 
 
-	//TEXT ANIMATION 
+	//TEXT ANIMATION
 	$('.tlt').textillate({
 	  // the default selector to use when detecting multiple texts to animate
 	  selector: '.texts',
-	
+
 	  // enable looping
 	  loop: true,
-	
+
 	  // sets the minimum display time for each text before it is replaced
 	  minDisplayTime: 2000,
-	
+
 	  // sets the initial delay before starting the animation
-	  // (note that depending on the in effect you may need to manually apply 
+	  // (note that depending on the in effect you may need to manually apply
 	  // visibility: hidden to the element before running this plugin)
 	  initialDelay: 0,
-	
+
 	  // set whether or not to automatically start animating
 	  autoStart: true,
-	
+
 	  in: {
 		// set the effect name
 		effect: 'fadeIn',
-	
+
 		// set the delay factor applied to each consecutive character
 		delayScale: 1,
-	
+
 		// set the delay between each character
 		delay: 50,
-	
+
 		// set to true to animate all the characters at the same time
 		sync: true,
-	
-		// randomize the character sequence 
+
+		// randomize the character sequence
 		// (note that shuffle doesn't make sense with sync = true)
 		shuffle: false,
-	
-		// reverse the character sequence 
+
+		// reverse the character sequence
 		// (note that reverse doesn't make sense with sync = true)
 		reverse: false,
-	
+
 		// callback that executes once the animation has finished
 		callback: function () {}
 	  },
-	
+
 	  // out animation settings.
 	  out: {
 		effect: 'fadeOut',
@@ -69,17 +69,17 @@ $(document).ready(function() {
 		reverse: false,
 		callback: function () {}
 	  },
-	
-	  // callback that executes once textillate has finished 
+
+	  // callback that executes once textillate has finished
 	  callback: function () {},
-	
+
 	  // set the type of token to animate (available types: 'char' and 'word')
 	  type: 'char'
 	});
-	
-	});
-	
-	
+
+});
+
+
 	//CONTACT FORM
 	$('#contact-form').validate({
     	rules: {
@@ -91,18 +91,18 @@ $(document).ready(function() {
             required: true,
             email: true
             },
-						
+
 					phone: {
             required: true,
             minlength: 2
             },
-            
+
           message: {
             required: true,
             minlength: 10
             }
         	},
-					
+
      messages: {
             name: "<i class='fa fa-exclamation-triangle'></i>Please specify your name.",
             email: {
@@ -110,7 +110,7 @@ $(document).ready(function() {
                 email: "<i class='fa fa-exclamation-triangle'></i>Please enter a valid email address."
             },
 						phone:"<i class='fa fa-exclamation-triangle'></i>Please enter a valid email address.",
-						
+
             message: "<i class='fa fa-exclamation-triangle'></i>Please enter your message."
         },
         submitHandler: function(form) {
@@ -134,23 +134,27 @@ $(document).ready(function() {
                 }
             });
         }
-    });	
-	
-	
-	//MAIL CHIMP	
-	$('#mc-form').ajaxChimp({
-		language: 'cm',
-		url: 'http://appengg.us12.list-manage2.com/subscribe/post?u=1262aa2502f3201fcdc20da64&amp;id=547618041b'// type your subcribe post id
-	  });
-		
-	$.ajaxChimp.translations.cm = {
-		'submit': 'Please Wait few sec.......',
-		 0: '<i class="fa fa-envelope"></i> Awesome! We have sent you a confirmation email',
-		 1: '<i class="fa fa-exclamation-triangle"></i> Please enter a value',
-		 2: '<i class="fa fa-exclamation-triangle"></i> An email address must contain a single @',
-		 3: '<i class="fa fa-exclamation-triangle"></i> The domain portion of the email address is invalid (the portion after the @: )',
-		 4: '<i class="fa fa-exclamation-triangle"></i> The username portion of the email address is invalid (the portion before the @: )',
-		 5: '<i class="fa fa-exclamation-triangle"></i> This email address looks fake or invalid. Please enter a real email address'
-	}; 
-	  
-		
+    });
+
+
+//MAIL CHIMP
+$('#mc-form').ajaxChimp({
+	language: 'cm',
+	url: 'http://appengg.us12.list-manage2.com/subscribe/post?u=1262aa2502f3201fcdc20da64&amp;id=547618041b'// type your subcribe post id
+  });
+
+$.ajaxChimp.translations.cm = {
+	'submit': 'Please Wait few sec.......',
+	 0: '<i class="fa fa-envelope"></i> Awesome! We have sent you a confirmation email',
+	 1: '<i class="fa fa-exclamation-triangle"></i> Please enter a value',
+	 2: '<i class="fa fa-exclamation-triangle"></i> An email address must contain a single @',
+	 3: '<i class="fa fa-exclamation-triangle"></i> The domain portion of the email address is invalid (the portion after the @: )',
+	 4: '<i class="fa fa-exclamation-triangle"></i> The username portion of the email address is invalid (the portion before the @: )',
+	 5: '<i class="fa fa-exclamation-triangle"></i> This email address looks fake or invalid. Please enter a real email address'
+};
+
+$("#mc-github-id").on('click blur keydown keyup keypress change',function(){
+  var textWrite = $("#mc-github-id").val();
+  $("#gg-img-tag").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '">');
+  $("#gg-img-tag-option").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '?rotate=270&width=568&height=88">');
+});
