29f67e9a4cb9a6f387b2ceae7e99b08747db2614
a-know <a.know.3373@gmail.com>
2016-03-07 09:32:23 +0900

    Insert personal data and little adjustment

--- public/renew/js/derek.js
+++ public/renew/js/derek.js
@@ -8,20 +8,20 @@ $(document).ready(function(){
     window.setTimeout(function() {
 
         $(window).trigger('resize');
-    
+
     }, 1000);
 
 });
 
 
 function init_masonry(){
-    
+
     var $container = $('#portfolio');
     var gutter = 12;
     var min_width = 215;
 
     $container.imagesLoaded( function(){
-        
+
         $container.masonry({
 
             itemSelector: '.portfolio-item',
@@ -44,39 +44,6 @@ function init_masonry(){
         });
     });
 }
-
-
-/* google maps */
-var myLatlng = new google.maps.LatLng(40.758890,-73.979366);
-
-var mapInitOpts = {
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: myLatlng,
-    zoom: 12
-};
-
-var map = new google.maps.Map(document.getElementById("my-location"), mapInitOpts);
-
-var marker = new google.maps.Marker({
-    position: myLatlng, 
-    map: map, 
-    title: "My Office" 
-});
-
-$(window).on('resize', function (e){
-    
-    center_map();
-
-});
-
-function center_map(){
-    
-    window.setTimeout(function() {
-        map.panTo(marker.getPosition()); 
-        }, 500);
-
-};
-
 
 /* tooltip */
 $('.my-tooltip').tooltip()
