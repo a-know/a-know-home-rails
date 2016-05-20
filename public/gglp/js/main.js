849a030ab747c618e49830fa8a6217964ffc561f
a-know <a.know.3373@gmail.com>
2016-05-20 09:43:29 +0900

    Abolish detail type

--- public/gglp/js/main.js
+++ public/gglp/js/main.js
@@ -20,7 +20,6 @@ $("#mc-github-id").on('blur keydown keyup keypress change',function(){
   var textWrite = $("#mc-github-id").val();
   $("#gg-img-tag").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '.png">');
   $("#gg-img-tag-option").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '.png?rotate=270&width=568&height=88">');
-  $("#gg-img-tag-detail").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '.png?type=detail">');
 });
 
 $("#generate-btn").on('click',function(){
@@ -32,15 +31,4 @@ $("#generate-btn").on('click',function(){
   var img_element = document.createElement('img');
   img_element.setAttribute("src", "https://grass-graph.shitemil.works/images/" + $("#mc-github-id").val() + ".png");
   $("#gg-img-area").append(img_element);
-});
-
-$("#generate-btn").on('click',function(){
-  if ($("#mc-github-id").val() == "") {
-    return
-  }
-  $("#gg-img-area-detail").empty();
-  $("#gg-img-area-detail").append("<h2 class='description'><small>type=detail</small></h2>")
-  var img_element = document.createElement('img');
-  img_element.setAttribute("src", "https://grass-graph.shitemil.works/images/" + $("#mc-github-id").val() + ".png?type=detail");
-  $("#gg-img-area-detail").append(img_element);
 });
