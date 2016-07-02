671ec36fcdd5cdcb91edf6593055251c94688264
a-know <a.know.3373@gmail.com>
2016-07-02 13:21:50 +0900

    JS の修正を忘れていた

--- public/gglp/js/main.js
+++ public/gglp/js/main.js
@@ -20,6 +20,7 @@ $("#mc-github-id").on('blur keydown keyup keypress change',function(){
   var textWrite = $("#mc-github-id").val();
   $("#gg-img-tag").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '.png">');
   $("#gg-img-tag-option").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '.png?rotate=270&width=568&height=88">');
+  $("#gg-img-tag-date-option").val('<img src="https://grass-graph.shitemil.works/images/' + textWrite + '.png?date=20160701">');
 });
 
 $("#generate-btn").on('click',function(){
