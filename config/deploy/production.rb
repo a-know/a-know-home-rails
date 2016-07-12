5b41b7a307c564a262fb9ba2955d39f3ddfdbc27
a-know <a.know.3373@gmail.com>
2016-07-12 11:53:56 +0900

    Mackerel API を利用してホスト一覧を取得する

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,24 @@
+require 'mackerel/client'
+
+set :mackerel_api_key, ENV['MACKEREL_API_KEY']
+set :service, ENV['MACKEREL_SERVICE_NAME']
+set :user, 'a-know'
+
+@client = Mackerel::Client.new(mackerel_api_key: fetch(:mackerel_api_key))
+
+def host_ip_addrs(role)
+  hosts = @client.get_hosts(service: fetch(:service), roles: fetch(:role)).select do |host|
+    host.status === 'standby' || host.status === 'working'
+  end.map do |host|
+    interface = host.interfaces.find { |i| /^eth/ === i['name'] }
+    interface['ipAddress'] if interface
+  end.select { |ipaddr| ipaddr != nil }.map do |ipaddr|
+    "#{fetch(:user)}@#{ipaddr}"
+  end
+end
+
 set :branch, 'master'
 
 set :rails_env, :production
-set :user, 'a-know'
 role :web, 'home.a-know.me'
+
